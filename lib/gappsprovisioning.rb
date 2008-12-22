98f450f9138eb93f31449708e7573f655b780531
Peter Zingg <peter@zingg.name>
2008-12-22 15:01:08 -0800

    sources

---
+++ lib/gappsprovisioning.rb
@@ -0,0 +1,14 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+  
+require 'net/https'
+require 'cgi'
+require 'rexml/document'
+
+require 'gappsprovisioning/connection'
+require 'gappsprovisioning/exceptions'
+require 'gappsprovisioning/provisioningapi'
+
+module GAppsProvisioning
+  VERSION = '0.0.2'
+end
